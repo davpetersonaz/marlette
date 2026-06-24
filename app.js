e86f82a3b4d566b9cfa2ff4dda0bdc4c36c2f498
Dave Peterson <davpeterson@zoho.com>
2026-06-24 10:48:41 -0700

    add forrest testimonials page

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.get('/meet-the-team', (req, res) => res.render('meet-the-team'));
 app.get('/accomplishments', (req, res) => res.render('accomplishments'));
 app.get('/whats-a-pc', (req, res) => res.render('whats-a-pc'));
 app.get('/negative-claims', (req, res) => res.render('negative-claims'));
+app.get('/testimonials', (req, res) => res.render('testimonials'));
 app.get('/contact', contactLimiter, async (req, res) => {
 	res.render('contact', { 
 		submitted: req.query.submitted === 'true',
